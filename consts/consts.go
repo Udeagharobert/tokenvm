2023dbdcc69aaf8eb75ef736beda9c1a6eccb61c
Udeagharobert <Udeagharobert2@gmail.com>
2024-07-03 18:10:47 +0000

    Robert

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Robert"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "RobNet"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "Rob"
 )
 
 var ID ids.ID
